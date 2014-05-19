6f782149f12a4909a20ab6aa986069554260db75
Bryan Pecjo <bryan@mba11-glitch.localdomain>
2014-05-19 15:39:45 +0800

    add Facebook timeline view controller

---
+++ StudySocialApp-iOS/StudySocialApp-iOS/FacebookAccount.m
@@ -0,0 +1,13 @@
+//
+//  FacebookAccount.m
+//  StudySocialApp-iOS
+//
+//  Created by Bryan Pecjo on 5/19/14.
+//  Copyright (c) 2014 Huchcode. All rights reserved.
+//
+
+#import "FacebookAccount.h"
+
+@implementation FacebookAccount
+
+@end
---
+++ StudySocialApp-iOS/StudySocialApp-iOS/FacebookTimeline.h
@@ -0,0 +1,13 @@
+//
+//  FacebookTimeline.h
+//  StudySocialApp-iOS
+//
+//  Created by Bryan Pecjo on 5/19/14.
+//  Copyright (c) 2014 Huchcode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FacebookTimeline : UITableViewController
+
+@end
